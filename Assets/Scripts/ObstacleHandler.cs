d8e25e2c49415205cc01e5307d2c6da9386264c7
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-07-13 19:20:18 +0700

    Add Player Collision

--- Assets/Scripts/ObstacleHandler.cs
+++ Assets/Scripts/ObstacleHandler.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class ObstacleHandler : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float destroyDistance = -7f; // Distance at which the obstacle will be destroyed
 
     void Update()
     {
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        if (transform.position.x <= destroyDistance) {
+            Destroy(gameObject); // Destroy the obstacle when it moves past the destroy distance
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,16 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         if (Input.GetMouseButton(0)) { 
             rb.linearVelocityY = jumpForce; // Set the vertical velocity to jumpForce
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameObject.SetActive(false); // Deactivate the obstacle on collision
     }
 }
